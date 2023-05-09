316cb0eba61bd12478aa87aa8eb07282f227586d
Fernando Dornelles Rocha <fe.dornellesrocha@outlook.com>
2023-05-08 23:13:10 -0300

    feat: functions to order by name, price and adding 10 percent to value products

--- intermediary/find_first_duplicate_char.py
+++ intermediary/find_first_duplicate_char.py
@@ -9,6 +9,7 @@ def find_first_duplicated(arr: list):
 
     return first_number_duplicated
 
+
 print(find_first_duplicated([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
 print(find_first_duplicated([9, 1, 8, 9, 9, 7, 2, 1, 6, 8]))
 print(find_first_duplicated([1, 3, 2, 2, 8, 6, 5, 9, 6, 7]))
---
+++ intermediary/order_products.py
@@ -0,0 +1,31 @@
+from copy import deepcopy
+
+products = [
+  {"nome": "Produto 5", "preco": 10.00},
+  {"nome": "Produto 1", "preco": 22.32},
+  {"nome": "Produto 3", "preco": 10.11},
+  {"nome": "Produto 2", "preco": 105.87},
+  {"nome": "Produto 4", "preco": 69.90},
+]
+
+
+def increase_10_percent(arr):
+    new_products = deepcopy(arr)
+    for product in new_products:
+        product["preco"] = round(product["preco"] * 1.10, 2)
+    return new_products
+
+
+def order_by_name(arr):
+    array = deepcopy(arr)
+    array.sort(key=lambda product: product["nome"], reverse=True)
+    return array
+
+
+def order_by_price(arr):
+    array = deepcopy(arr)
+    array.sort(key=lambda product: product["preco"])
+    return array
+
+
+print(order_by_price(products))

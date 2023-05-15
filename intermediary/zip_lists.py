9ec3650da539324e3cdfd46ea897871e503e9c62
Fernando Dornelles Rocha <fe.dornellesrocha@outlook.com>
2023-05-15 19:36:18 -0300

    fix: name state altough capitals

--- intermediary/zip_lists.py
+++ intermediary/zip_lists.py
@@ -1,5 +1,5 @@
-def zip_lists(cities, capitals):
-    result = [(cities[i], capitals[i]) for i in range(len(cities))]
+def zip_lists(cities, states):
+    result = [(cities[i], states[i]) for i in range(len(cities))]
     return result
 
 

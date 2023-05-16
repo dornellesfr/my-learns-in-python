202d1c20fb5776bae22a371664e3e62ba55c230b
Fernando Dornelles Rocha <fe.dornellesrocha@outlook.com>
2023-05-15 22:07:16 -0300

    feat: add function to sum lists

--- intermediary/zip_lists.py
+++ intermediary/zip_lists.py
@@ -1,9 +1,24 @@
-def zip_lists(cities, states):
-    result = [(cities[i], states[i]) for i in range(len(cities))]
+def zip_lists(l1: list, l2: list):
+    interval = min(len(l1), len(l2))
+    result = [(l1[i], l2[i]) for i in range(interval)]
+    return result
+
+
+def list_with_all_values(l1: list, l2: list):
+    result = [x + y for x, y in zip(l1, l2)]
     return result
 
 
 if __name__ == "__main__":
-    a = zip_lists(['Salvador', 'Ubatuba', 'Belo Horizonte'],
-                  ['BA', 'SP', 'MG', 'RJ'])
+    lis1 = ['Salvador', 'Ubatuba', 'Belo Horizonte']
+    lis2 = ['BA', 'SP', 'MG', 'RJ']
+
+    lis3 = [1, 2, 3, 4, 5, 6, 7]
+    lis4 = [1, 2, 3, 4]
+
+    a = zip_lists(lis1, lis2)
+
     print(a)
+    print(list(zip(lis1, lis2)))
+    b = list_with_all_values(lis3, lis4)
+    print(b)

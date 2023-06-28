774e6cc8dbd46d0862e60a612e923016bbc0b7da
Fernando Dornelles Rocha <fe.dornellesrocha@outlook.com>
2023-06-27 21:51:06 -0300

    treating exception on my context manager

--- intermediary/my_own_context_manager.py
+++ intermediary/my_own_context_manager.py
@@ -9,13 +9,21 @@ class MyContextManager:
         '''Retorno do enter entra pra variável do fim da linha do with'''
         print('Opening file')
         self._file = open(self.path, self.mode, encoding='utf-8')
-        return self.file
+        return self._file
 
     def __exit__(self, class_exception, exception_, traceback_):
         print('Closing file')
         self._file.close()
 
+        print(class_exception)
+        print(exception_)
+        print(traceback_)
 
-instance = MyContextManager('path_file', 'w')
+        return True  # The exception was dealed
+
+
+instance = MyContextManager('path_file.txt', 'w')
 with instance as something:
-    ...
+    something.write('Hi\n')
+    something.write('Hi2\n')
+    something.write('Hi3\n')
